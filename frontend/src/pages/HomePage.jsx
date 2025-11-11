7f8427f4f9ab0d5b0da2e3c127705139c3484db0
Yeksuh_ <john_vasko@hotmail.com>
2025-11-10 22:17:58 -0500

    Fix home page attempt 3

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -38,17 +38,21 @@ export default function HomePage() {
       <div className="tire-marks" />
       
       {/* Animated background elements */}
-      <div className="relative logo-float w-[48rem] h-[48rem] mx-auto">
+      {/* Hero Logo Section */}
+      <div className="relative w-[48rem] h-[48rem] mx-auto logo-float">
+        {/* Main Logo */}
         <img 
           src="https://customer-assets.emergentagent.com/job_jdm-legends-2/artifacts/3debsbpu_fixedcolourskull.png"
           alt="Triple Barrel Racing Logo"
           className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[32rem] sm:w-[40rem] lg:w-[48rem] h-auto logo-glow"
           data-testid="logo-image"
         />
+      
+        {/* Text Logo */}
         <img 
           src="https://customer-assets.emergentagent.com/job_jdm-legends-2/artifacts/ehzw5qae_fixedwhitetext.png"
           alt="Triple Barrel Racing Text"
-          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-72 sm:w-96 lg:w-[28rem] h-auto logo-glow"
+          className="absolute top-[28rem] left-1/2 transform -translate-x-1/2 w-72 sm:w-96 lg:w-[28rem] h-auto logo-glow"
           data-testid="logo-text"
         />
       </div>
